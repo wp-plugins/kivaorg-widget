9b5e4efb75f5ce0aa44908bd83838f518f2c2fa1
road2nowhere <road2nowhere@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-02-06 19:02:02 +0000

    Adding first version
    git-svn-id: https://plugins.svn.wordpress.org/kivaorg-widget/trunk@92420 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ kiva.php
@@ -0,0 +1,160 @@
+<?php
+/*
+Plugin Name: Kiva Widget
+Plugin URI: http://kiva.com
+Description: Kiva widget, display my investments
+Author: Marilyn Burgess
+Author URI: http://urpisdream.com
+*/
+
+/*
+  Copyright 2009 Urpi's Dream
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+function widget_kiva_loan_init() {
+
+	if ( !function_exists('register_sidebar_widget') || !function_exists('register_widget_control') ){
+		return; 
+    }
+
+	// Print the sidebar widget
+	function widget_kiva_loan($args) {
+		extract($args);
+
+		// Collect our widget's options, or define their defaults.
+		$title = 'Kiva loans';
+		$text = get_kiva_loans();
+
+		echo $before_widget;
+		echo $before_title . $title . $after_title;
+		echo $text;
+		echo $after_widget;
+	}
+
+    function get_kiva_loans(){
+
+        $options = get_option('widget_kiva_loan');
+        $limit = $options['number_of_loans'];
+        $size = $options['image_size'];
+        $username = $options['kiva_username'];
+
+        $content = "";
+        if(! $username){
+            $content = "<p>No user specified!</p>";
+        }else{
+            $url = "http://api.kivaws.org/v1/lenders/$username/loans.json";
+    
+            $ch = curl_init();
+    
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            $response = curl_exec ($ch);
+            curl_close ($ch);
+            $results = json_decode($response);
+    
+            $loans_array = $results->{'loans'};
+            $total_loans = sizeof($loans_array);
+
+            if($total_loans == 0){
+                $content = "<p>User has no loans!</p>";
+            }else{
+                $content = "<p>I have microlent to:</p>";     
+            
+                // Make this randomely choose one 
+                $loan_count = 0;
+                $used = array_fill(0, $total_loans-1, 0);
+                for($i = 0; $i < $total_loans; $i++){
+                    if($i < $limit){
+                        $j = rand(0, $total_loans-1 );
+                        while ($used[$j]) {
+                            $j = rand(0, $total_loans-1 );
+                        }   
+                        $used[$j] = 1;
+                        $loan = $loans_array[$j];
+                        $content .= build_html($loan, $size);
+                        $loan_count++;
+                    }else{
+                        break;
+                    }
+                }                
+            }
+        }
+
+        $html = "<div id='kiva_loans'>"; 
+        $html .= $content;
+        $html .= "<center><a href='http://kiva.org/'><img src='/wp-content/plugins/kiva-widget/kiva.gif' alt='Kiva.org' class='kiva_logo' /></a></center>";
+        $html .= "</div>";
+        return $html;
+    }
+
+    function build_html($loan, $size){
+
+        $html = "<link rel='stylesheet' href='". get_option('siteurl') . "/wp-content/plugins/kiva-widget/style.css' type='text/css' media='screen' />";
+        $html .= "<div class='loan'>";
+        $html .= "<h3>" . $loan->{name}. "</h3><br />";
+        $img_source = "http://www.kiva.org/img/$size/" . $loan->{'image'}->{'id'} . ".jpg";
+        $html .= "<a href='http://www.kiva.org/app.php?page=businesses&action=about&id=" . $loan->{id} . "' target='_new'><img src='$img_source' alt='". $loan->{name} . "' /></a><br />";
+        $html .= "<table>";
+        $html .= "<tr><td style='vertical-align:top'><b>Location:</b></td><td>". $loan->{location}->{country} . ", " . $loan->{location}->{town} . "</td></tr>";
+        $html .= "<tr><td style='vertical-align:top'><b>Activity:</b></td><td>" . $loan->{activity} . "</td></tr>";
+        $html .= "</table><br />";
+        $html .= "</div>";
+
+        return $html;
+    }
+
+	function widget_kiva_loan_control() {
+		$options = get_option('widget_kiva_loan');
+
+		if ( $_POST['kiva_loan_submit'] ) {
+			$newoptions['kiva_username'] = strip_tags(stripslashes($_POST['kiva_username']));
+			$newoptions['number_of_loans'] = strip_tags(stripslashes($_POST['number_of_loans']));
+            $newoptions['image_size'] = strip_tags(stripslashes($_POST['image_size']));
+
+    		if ( $options != $newoptions ) {
+    			$options = $newoptions;
+                echo "updating options <br />";
+    			update_option('widget_kiva_loan', $options);
+    		}
+        }
+
+		$username = htmlspecialchars($options['kiva_username'], ENT_QUOTES);
+		$number_of_loans = htmlspecialchars($options['number_of_loans'], ENT_QUOTES);
+        $image_size = htmlspecialchars($options['image_size'], ENT_QUOTES);
+
+// The admin control for the widget
+?>
+		<div>
+		<label for="kiva_username" style="line-height:35px;display:block;">Kiva Username: <input type="text" id="kiva_username" name="kiva_username" value="<?php echo $username ?>" /></label>
+		<label for="number_of_loans" style="line-height:35px;display:block;">Number of loans: <input type="text" id="number_of_loans" name="number_of_loans" value="<?php echo $number_of_loans; ?>" /></label>
+        <label for="image_size" style="line-height:35px;display:block;">Image size: <br /><input type="text" id="image_size" name="image_size" value="<?php echo $image_size; ?>" /></label>
+		<input type="hidden" name="kiva_loan_submit" id="kiva_loan_submit" value="1" />
+		</div>
+	<?php
+	}
+
+	// This registers the widget. 
+	register_sidebar_widget('Kiva loans', 'widget_kiva_loan');
+
+	// This registers the widget control form.
+	register_widget_control('Kiva loans', 'widget_kiva_loan_control');
+}
+
+// Delays plugin execution until Dynamic Sidebar has loaded first.
+add_action('plugins_loaded', 'widget_kiva_loan_init');
+
+?>
---
+++ readme.txt
@@ -0,0 +1,40 @@
+=== Kiva.org Loans Widget ===
+Contributors: road2nowhere
+Donate link: http://urpisdream.com/
+Tags: kiva, widgit
+Requires at least: 2.2
+Tested up to: 2.7
+Stable tag: 4.3
+
+Displays rotating entrepreneurs the blogger has invested in publically. Links back to Kiva.
+
+== Description ==
+
+Kiva Lendees is a sidebar widget. The user specifies their Kiva username, the number of lendees (n) 
+to show, and the size of the images that will be displayed. The widget then randomly selects n of the 
+user's lendees from the Kiva API, and displays breif info about each of them. Links to the Kiva.org website
+
+
+== Installation ==
+
+How to install the Kiva Lendees plugin:
+
+1. Upload `kiva-widget` directory to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Add the widget to a sidebar, and set the options 
+
+== Frequently Asked Questions ==
+
+= Will it show private loans? =
+
+No, it will only show your public Kiva loans.
+
+= How do I change the look of the Kiva Loans Widget? =
+
+The style sheet is in the pluing folder at /wp-content/plugins/kiva-widget/style.css
+
+== Screenshots ==
+
+
+
+
