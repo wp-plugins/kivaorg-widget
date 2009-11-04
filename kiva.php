8fe7cc05ed6a44c343889c681b820513ef970cba
road2nowhere <road2nowhere@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-11-04 04:36:37 +0000

    tagging version 2.0
    git-svn-id: https://plugins.svn.wordpress.org/kivaorg-widget/tags/2.9@170240 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- kiva.php
+++ kiva.php
@@ -3,7 +3,7 @@
 Plugin Name: Kiva Widget
 Plugin URI: http://urpisdream.com/2009/05/kiva-loans-wordpress-widget/
 Description: Kiva widget, display my investments
-Version: 2.7
+Version: 2.9
 Author: Marilyn Burgess
 Author URI: http://urpisdream.com
 */
@@ -25,6 +25,9 @@ Author URI: http://urpisdream.com
     along with this program; if not, write to the Free Software
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
+
+global $debug;
+$debug = 0;
 
 global $kiva_cache_dir;
 $kiva_cache_dir = ( defined('WP_CONTENT_DIR') ) ? WP_CONTENT_DIR : ABSPATH . 'wp-content';
@@ -59,6 +62,14 @@ function widget_kiva_loan_init() {
 
     function get_kiva_loans(){
         global $kiva_api_app_id;
+        global $debug;
+
+        if($debug == 1){
+            echo "<b>Please pardon the temporary mess!</b><br />";
+            echo "in get_kiva_loans<br />";
+            flush();
+        }
+
         $options = get_option('widget_kiva_loan');
         $limit = $options['number_of_loans'];
         if($limit <  1){
@@ -78,24 +89,45 @@ function widget_kiva_loan_init() {
             if(is_current_kiva_cache_available()){
                 $results = get_kiva_cache();
             }
+            if($debug == 1){
+                echo "is_current_kiva_cache_available??  $results<br />";
+                flush();
+            }
 
+            $total_loans = 0;
             if($results == ""){
                 $url = "http://api.kivaws.org/v1/lenders/$username/loans.json?app_id=$kiva_api_app_id";
-        
+                if($debug == 1){
+                    echo "url: $url<br />";
+                    flush();
+                }
                 $ch = curl_init();
                 curl_setopt($ch, CURLOPT_URL, $url);
                 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+                curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 3);
                 $response = curl_exec ($ch);
                 curl_close ($ch);
-                $results = json_decode($response);
 
-                //echo "going to write results: <br />";
-                write_kiva_cache($response);
+                if($response != ""){
+                    $results = json_decode($response);
+
+                    if($debug == 1){
+                        echo "going to write results: $results <br />";
+                        flush();
+                    }
+                    write_kiva_cache($response);
+                }else{
+                    $total_loans = -1;
+                }
             }
-            $loans_array = $results->{'loans'};
-            $total_loans = sizeof($loans_array);
+            if($total_loans != -1){
+                $loans_array = $results->{'loans'};
+                $total_loans = sizeof($loans_array);
+            }
 
-            if($total_loans <= 0){
+            if($total_loans == -1){
+                $content = "<p>There was an error in retriving the loans from Kiva. Please try again later</p>";
+            }else if($total_loans <= 0){
                 $content = "<p>User has no loans!</p>";
             }else{
                 $content = "<p>I have microlent to:</p>";     
@@ -129,15 +161,21 @@ function widget_kiva_loan_init() {
 
     function is_current_kiva_cache_available(){
         global $kiva_cache_dir;
+        global $debug;
+
+        if($debug == 1){
+            echo "in is_current_kiva_cache_available<br />";
+            flush();
+        }
 
         $cache_file = "";
         if (is_dir($kiva_cache_dir)) {
             if ($dh = opendir($kiva_cache_dir)) {
                 while (($file = readdir($dh)) !== false) {
-		    if(! is_dir($file)){
-			if( preg_match("/^kiva_cache_/", $file)){
-	                     $cache_file = $file;
-			}
+                    if(! is_dir($file)){
+                        if( preg_match("/^kiva_cache_/", $file)){
+    	                     $cache_file = $file;
+                        }
                     }
                 }
             }
@@ -148,20 +186,51 @@ function widget_kiva_loan_init() {
             $cache_time = preg_replace("/kiva_cache_(\d+)\.txt/", "$1", $cache_file);
             $time = time();
 
-            if($time - $cache_time < (60 * 60)){
+            if($time - $cache_time < (60)){
+            #if($time - $cache_time < (60 * 60)){
                 // Cache is less than an hour old
                 return 1;
             }else{
                 // delete the old cache
                 $file = "$kiva_cache_dir/$cache_file_original";
                 unlink($file);
+                check_for_old_cache_files();
             }
         }
         return 0;    
     }
 
+    function check_for_old_cache_files(){
+        global $kiva_cache_dir;
+
+        $i = 0;
+        $limit = 1000;
+        if (is_dir($kiva_cache_dir)) {
+            if ($dh = opendir($kiva_cache_dir)) {
+                while (($file = readdir($dh)) !== false) {
+                    if(! is_dir($file)){
+                        if( preg_match("/^kiva_cache_/", $file)){
+                            // delelte the file
+                            unlink("$kiva_cache_dir/$file");
+                        }
+                    }
+                    $i++;
+                    if($i == $limit){
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
     function get_kiva_cache(){
         global $kiva_cache_dir;
+        global $debug;
+
+        if($debug == 1){
+            echo "in get_kiva_cache<br />";
+            flush();
+        }
 
         $cache_file = "";
         if (is_dir($kiva_cache_dir)) {
@@ -178,9 +247,16 @@ function widget_kiva_loan_init() {
 
         $cache_file_name = $kiva_cache_dir . "/" . $cache_file;
 
-        $fh = fopen($cache_file_name, 'r') or die("can't open file: " . $cache_file_name);
-        $response = fread($fh, filesize($cache_file_name));
-        fclose($fh);
+        if($debug == 1){
+            echo "cache_file_name: $cache_file_name<br />";
+            flush();
+        }
+
+        if(filesize($cache_file_name) > 0){ 
+            $fh = fopen($cache_file_name, 'r') or die("can't open file: " . $cache_file_name);
+            $response = fread($fh, filesize($cache_file_name));
+            fclose($fh);
+        }       
 
         $cache_file = json_decode($response);
 
@@ -189,7 +265,17 @@ function widget_kiva_loan_init() {
 
     function write_kiva_cache($json_response){
         global $kiva_cache_dir;
-      
+        global $debug;
+
+        if($debug == 1){
+            echo "in write_kiva_cache with content: $json_response<br />";
+            flush();
+        }
+
+        if($json_response == ""){
+            return;
+        }
+
         $time = time();
         $cache_file_name = "$kiva_cache_dir/kiva_cache_$time".".txt";
 
@@ -217,12 +303,15 @@ function widget_kiva_loan_init() {
             curl_setopt($ch, CURLOPT_URL, $url);
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
             curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+            curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 3);
             $image = curl_exec ($ch);
             curl_close ($ch);
 
-            $fh = fopen($image_path, 'w') or die("can't open file: " . $image_path);
-            fwrite($fh, $image);
-            fclose($fh);
+            if($image != ""){
+                $fh = fopen($image_path, 'w') or die("can't open file: " . $image_path);
+                fwrite($fh, $image);
+                fclose($fh);
+            }
         }else{
             # Check for moved images for those empty images already retrieved
             $fh = fopen($image_path, 'r');
@@ -238,12 +327,15 @@ function widget_kiva_loan_init() {
                     $ch = curl_init();
                     curl_setopt($ch, CURLOPT_URL, $movedImageURL[1]);
                     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+                    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 3);
                     $image = curl_exec ($ch);
                     curl_close ($ch);
 
-                    $fh = fopen($image_path, 'w') or die("can't open file: " . $image_path);
-                    fwrite($fh, $image);
-                    fclose($fh);
+                    if($image != ""){
+                        $fh = fopen($image_path, 'w') or die("can't open file: " . $image_path);
+                        fwrite($fh, $image);
+                        fclose($fh);
+                    }
                 }
             }
         }
